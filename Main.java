7290c73c7016bfa1b3498988128baf1dd7b8aca0
SabbirAhsan1 <sabbir.ahs010@gmail.com>
2024-10-09 12:22:14 +0600

    Magic number removed and added constant

--- Main.java
+++ Main.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        final byte percent = 100;
+        final byte numberOfMonths = 12;
         System.out.print("Principal: ");
         double principal = input.nextDouble();
         System.out.print("Annual Interest Rate: ");
         double interestRate = input.nextDouble();
         System.out.print("Period (Years): ");
         double period = input.nextDouble();
-        interestRate /= 100;
-        interestRate /= 12;
-        period *= 12;
+        interestRate = interestRate / percent / numberOfMonths ;
+        period *= numberOfMonths;
         double mortgage = (principal * (interestRate*Math.pow((1+interestRate), period))) / (Math.pow((1+interestRate), period) -1);
         String result = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + result);
