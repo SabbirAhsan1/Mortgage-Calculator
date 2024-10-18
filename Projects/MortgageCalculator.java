e1ace2217844d8c441ed77b77d771a71cddb3074
SabbirAhsan1 <sabbir.ahs010@gmail.com>
2024-10-18 10:50:58 +0600

    Created multiple methods to made this code cleaner.
    The main function only has a few lines now.  Previous features remain the same.

--- Projects/MortgageCalculator.java
+++ Projects/MortgageCalculator.java
@@ -4,34 +4,46 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+
+        double principal = readNumber("Principal: ", 1000, 1_000_000);
+        double interestRate = readNumber("Annual Interest Rate: ", 1, 30);
+        double period = readNumber("Period (Years): ", 1, 30);
+
+        double mortgage = calculateMortgage(principal, interestRate, period);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+    public static double readNumber(
+            String prompt,
+            double min,
+            double max){
+
         Scanner input = new Scanner(System.in);
+        double value;
+        while(true){
+            System.out.print(prompt);
+            value = input.nextDouble();
+            if(value >= min && value <=max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+
+    }
+
+
+    public static double calculateMortgage(
+            double principal,
+            double interestRate,
+            double period){
+
         final byte percent = 100;
         final byte numberOfMonths = 12;
-        System.out.print("Principal ($1K - $1M): ");
-        double principal = input.nextDouble();
-        while(principal<1000 || principal>1000000){
-                System.out.println("Enter a number between 1000 to 1000000");
-                System.out.print("Principal ($1K - $1M): ");
-                principal = input.nextDouble();
-        }
-        System.out.print("Annual Interest Rate: ");
-        double interestRate = input.nextDouble();
-        while(interestRate<=0 || interestRate>30){
-            System.out.println("Enter a value greater than 0 and less than or equal to 30");
-            System.out.print("Annual Interest Rate: ");
-            interestRate = input.nextDouble();
-        }
-        System.out.print("Period (Years): ");
-        double period = input.nextDouble();
-        while(period<1 || period>30){
-            System.out.println("Enter a value between 1 and 30");
-            System.out.print("Period (Years): ");
-            period = input.nextDouble();
-        }
         interestRate = interestRate / percent / numberOfMonths ;
         period *= numberOfMonths;
         double mortgage = (principal * (interestRate*Math.pow((1+interestRate), period))) / (Math.pow((1+interestRate), period) -1);
-        String result = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
-        System.out.println("Mortgage: " + result);
+        return mortgage;
+
     }
 }
